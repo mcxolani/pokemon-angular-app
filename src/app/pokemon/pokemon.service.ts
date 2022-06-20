686c9d567ff11c7a445f4bfc5275fdf880b1451f
xolani <mcxolani@gmail.com>
2022-06-20 04:52:22 +0200

    add all

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Pokemon } from './pokemon';
 
 @Injectable({
@@ -10,7 +11,7 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  private url = 'http://localhost:3000/pokemon/';
+  private url = environment.apiURL + '/pokemon/';
 
   getAllPokemon(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>(this.url);
